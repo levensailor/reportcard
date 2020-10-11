7ada41432509488e3fe8a5095f5ab37d18f56a6b
Jeff Levensailor <jlevensailor@presidio.com>
2020-10-10 22:26:49 -0400

    first

---
+++ build/lib/io/io.py
@@ -0,0 +1,9 @@
+from requests import Session
+
+class io(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def test():
+        print('success')
